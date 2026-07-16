7d27c1338e3d1d36ee714ab890174c234cdeed59
Zanie Blue <contact@zanie.dev>
2026-07-15 21:33:40 -0500

    Replace vulnerable RSA test generation with a fixed fixture

---
+++ src/test_keys.rs
@@ -0,0 +1,3 @@
+pub(crate) const RSA_PRIVATE_KEY: &str = include_str!("../tests/fixtures/oidc-test-key.pem");
+pub(crate) const RSA_MODULUS: &str = "yF2Eec0Xe5vTXwHiApgLRLJGdV287nFVUZ58cYvfp1qOWqUD6e7vxbEx9SpskEbCmFB1vC0Js2rGpZphqkn0wIGV7zrHPNQFDZ8UNiJ2AJjiw3uDZKh6mp3VHkUnJu_rlSK4UxOBrGRHTB_n2UnHLbcOyMt8jcWvxwoneikfmman5BZ5YPSdgXlGfaStHSUsM1PDC-w0-a-LHCzF--slcemvnvfooQJ0QgNB_t1lwavcK-1IUlB5njaUK5-hRS5VUGnON9BbRsQUIec4VitiqsYRePiw5I7ukNa56Qq1XL6W-xRUnpMFAy4RKRRIwZWtD_X09_k00F1C-xiuya8T1Q";
+pub(crate) const RSA_EXPONENT: &str = "AQAB";
